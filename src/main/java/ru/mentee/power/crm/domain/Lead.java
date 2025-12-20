7452a72be28222dcd4016783c3a9fbbc551d91db
Bark Maxim <maximalen1999@gmail.com>
2025-12-21 02:27:11 +0300

    Добавил Equals/HashCode

--- src/main/java/ru/mentee/power/crm/domain/Lead.java
+++ src/main/java/ru/mentee/power/crm/domain/Lead.java
@@ -1,5 +1,7 @@
 package ru.mentee.power.crm.domain;
 
+import java.util.Objects;
+
 public class Lead {
     private String id;
     private String email;
@@ -16,7 +18,7 @@ public class Lead {
     }
 
     public String getId() {
-       return id;
+        return id;
     }
 
     public String getEmail() {
@@ -39,5 +41,22 @@ public class Lead {
     public String toString() {
         return "Lead{id = %s, email = %s, phone = %s, company = %s, status = %s }".
                 formatted(id, email, phone, company, status);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Lead lead = (Lead) o;
+        return Objects.equals(id, lead.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
