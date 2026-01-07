e36462f7477e2b78d674f88545ff2879fe5ef81d
Bark Maxim <maximalen1999@gmail.com>
2026-01-07 17:40:03 +0300

    BCORE-13: DONE!

--- src/main/java/ru/mentee/power/crm/servlet/LeadListServlet.java
+++ src/main/java/ru/mentee/power/crm/servlet/LeadListServlet.java
@@ -1,7 +1,9 @@
 package ru.mentee.power.crm.servlet;
 
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
+import gg.jte.output.PrintWriterOutput;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.DirectoryCodeResolver;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -10,54 +12,35 @@ import jakarta.servlet.http.HttpServletResponse;
 import ru.mentee.power.crm.model.Lead;
 import ru.mentee.power.crm.service.LeadService;
 
-
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @WebServlet("/leads")
 public class LeadListServlet extends HttpServlet {
 
+    private TemplateEngine templateEngine;
+
+    @Override
+    public void init() throws ServletException {
+        Path templatePath = Path.of("src/main/jte");
+        DirectoryCodeResolver codeResolver = new DirectoryCodeResolver(templatePath);
+        this.templateEngine = TemplateEngine.create(codeResolver,ContentType.Html);
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        System.out.println("GET /leads request received");
 
-        ServletContext context = getServletContext();
-        LeadService leadService = (LeadService) context.getAttribute("leadService");
-        List<Lead> leads = leadService.findAll();
+        LeadService service = (LeadService) getServletContext().getAttribute("leadService");
+        List<Lead> leads = service.findAll();
 
-        System.out.println("Found " + leads.size() + " leads");
+        Map<String, Object> model = new HashMap<>();
+        model.put("leads", leads);
 
         response.setContentType("text/html; charset=UTF-8");
-        PrintWriter writer = response.getWriter();
-        writer.println("<!DOCTYPE html>");
-        writer.println("<html>");
-        writer.println("<head><title>CRM - Lead List</title></head>");
-        writer.println("<body>");
-        writer.println("<h1>Lead List</h1>");
-        writer.println("<table border='1'>");
-        writer.println("<thead>");
-        writer.println("<tr>");
-        writer.println("<th>Email</th>");
-        writer.println("<th>Company</th>");
-        writer.println("<th>Status</th>");
-        writer.println("</tr>");
-        writer.println("</thead>");
-        writer.println("<tbody>");
-
-        for (Lead lead : leads) {
-            writer.println("<tr>");
-            writer.println("<td>" + lead.email() + "</td>");
-            writer.println("<td>" + lead.company() + "</td>");
-            writer.println("<td>" + lead.status() + "</td>");
-        }
-        writer.println("</tr>");
-        writer.println("</tbody>");
-        writer.println("</table>");
-        writer.println("</body>");
-        writer.println("</html>");
-        response.setStatus(200);
+        templateEngine.render("leads/list.jte", model, new PrintWriterOutput(response.getWriter()));
     }
 }
-
